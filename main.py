edfa0e4d17a11abb22c6de044dbc2144dde4a3d3
Replit user <>
2023-06-03 07:17:14 +0000

    add to env variabel private key

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from websites import create_app
+import random
 
 app = create_app()
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True)
+	app.run(host='0.0.0.0',  # Establishes the host, required for repl to detect the site
+		port=random.randint(2000, 9000)  # Randomly select the port the machine hosts on.
+	)
