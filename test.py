901ff63e17a3a363a1bc4bad2e161b7804272d53
Muhammad Al lail Qadrillah <mallailqadrillah43@gmail.com>
2023-06-06 21:58:27 +0700

    host server

--- test.py
+++
@@ -1,4 +0,0 @@
-perangkat_lainnya = {'remote': False, 'kabel vga': False, 'lensa pendukung': False,
-                     'kabel hdmi': True, 'kabel dvi': True, 'case pelindung': True, 'layar': True}
-
-print(" | ".join([key for key, value in perangkat_lainnya.items() if value]))
